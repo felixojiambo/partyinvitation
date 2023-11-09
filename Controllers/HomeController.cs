84bd0654e1a9bd22d3c01dedf7449c7388869523
Ojiamboloc <ojiamboloc@gmail.com>
2023-11-09 07:46:23 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index() {
+ return View();
+ }
+    [HttpGet]
+    public ViewResult RsvpForm()
+    {
+        return View();
+    }
+    [HttpPost]
+    public ViewResult RsvpForm(GuestResponse guestResponse)
+    {
+       
+        return View();
+    }
+}
